137ec8fb7a5892a10fa12700af74afb6452efa2b
Amos Gionata Neri <a.neri@prowebconsulting.net>
2023-11-16 11:16:52 +0100

    added scrape npm registry task

---
+++ app/Tasks/ScrapeNpmPackages.ts
@@ -0,0 +1,22 @@
+import {BaseTask, CronTimeV2} from 'adonis5-scheduler/build/src/Scheduler/Task'
+import Jobs from '@ioc:Providers/Jobs';
+import {ScrapeNpmRegistryJobParameters} from "App/Jobs/ScrapeNpmRegistryJob";
+
+export default class ScrapeNpmPackages extends BaseTask {
+  public static get schedule() {
+    return CronTimeV2.everyWeek();
+  }
+
+  public static get useLock() {
+    return true;
+  }
+
+  public async handle() {
+    await Jobs.waitUntilDone(
+      await Jobs.dispatch<ScrapeNpmRegistryJobParameters>(
+        "ScrapeNpmRegistryJob",
+        {}
+      )
+    );
+  }
+}
